bc239f919e3820c94f08f25ed19153ca233669cb
Jens Schauder <jens.schauder@freenet.de>
2011-08-11 18:31:18 +0200

    refactored table model tests to reuse a single check method

--- src/test/scala/de/schauderhaft/mtp/binding/TableBinderTest.scala
+++ src/test/scala/de/schauderhaft/mtp/binding/TableBinderTest.scala
@@ -1,53 +1,48 @@
 package de.schauderhaft.mtp.binding
 
 import de.schauderhaft.mtp.property._
-
 import javax.swing.{ Action => _, _ }
-
 import org.scalatest._
 import org.scalatest.matchers._
 import org.junit.runner.RunWith
 import org.scalatest.junit._
 import scala.collection.immutable._
+import javax.swing.table.TableModel
+
 @RunWith(classOf[JUnitRunner])
 class TableBinderTest extends FunSuite with ShouldMatchers {
 
-    test("binding an empty Seq Property to a JTable results in an empty table"){
+    /**
+     * checks a table model agains a sequence of values
+     */
+    def check(model : TableModel, values : Seq[AnyRef]) {
+        model.getColumnCount() should be(1)
+        model.getRowCount() should be(values.size)
+        for (i <- 0 to values.size - 1; v = values(i))
+            model.getValueAt(i, 0) should be(v)
+    }
+
+    test("binding an empty Seq Property to a JTable results in an empty table") {
         val property = new Property(Seq[String]())
         val table = new JTable
         Binder.bind(property, table)
-        
-        table.getModel().getRowCount() should be (0)
+ 
+        check(table.getModel, Seq())
     }
-    		
-     
-    test("binding an empty Seq Property to a JTable results in a single column table"){
-        val property = new Property(Seq[String]())
-        val table = new JTable
-        Binder.bind(property, table)
-        
-        table.getModel().getColumnCount() should be (1)
-    }
-    
-    test("binding a single element Seq[String] Property to a JTable results in that element in the single cell"){
+
+    test("binding a single element Seq[String] Property to a JTable results in that element in the single cell") {
         val property = new Property(Seq("hello"))
         val table = new JTable
         Binder.bind(property, table)
-        
-        table.getModel().getColumnCount() should be (1)
-        table.getModel().getRowCount() should be (1)
-        table.getModel().getValueAt(0,0) should be ("hello")
+
+        check(table.getModel(), Seq("hello"))
     }
-    
-    test("binding a n element Seq[String] Property to a JTable results in the elements being values of the cells of a single column"){
+
+    test("binding a n element Seq[String] Property to a JTable results in the elements being values of the cells of a single column") {
         val property = new Property(Seq("hallo", "ollah", "hi"))
         val table = new JTable
         Binder.bind(property, table)
-
-        table.getModel().getColumnCount() should be (1)
-        table.getModel().getRowCount() should be (3)
-        table.getModel().getValueAt(0,0) should be ("hallo")
-        table.getModel().getValueAt(1,0) should be ("ollah")
-        table.getModel().getValueAt(2,0) should be ("hi")
+        
+        check(table.getModel(), Seq("hallo","ollah","hi"))
     }
 }
